3ea8c8336a09bc6c590c4ca8cd346cedd84c0c30
timurci <timurcakmakoglu@gmail.com>
2025-07-02 22:03:26 +0300

    Updated dec module init

--- src/dec_torch/dec/__init__.py
+++ src/dec_torch/dec/__init__.py
@@ -1,5 +1,8 @@
-from .dec import DEC
-from .dec import init_clusters, init_clusters_random
+from .dec import DEC, KLDivLoss
+from .dec import init_clusters, init_clusters_random, init_clusters_trials
 from . import io  # noqa: F401
 
-__all__ = ["DEC", "init_clusters", "init_clusters_random"]
+__all__ = [
+    "DEC", "KLDivLoss",
+    "init_clusters", "init_clusters_random", "init_clusters_trials"
+]
